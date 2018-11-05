b27c7d73e02d2caca44c321dc2bbd5eec934c4e4
Melany Pelletier <melany.pelletier@gmail.com>
2018-11-05 11:30:14 -0500

    Code QUILT case study

--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@
 window.onscroll = function() {myFunction()};
 
 // Get the navbar
-var navbar = document.getElementById("navbar");
+var stick = document.getElementById("stick");
 
 // Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+var sticky = stick.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    stick.classList.add("sticky")
   } else {
-    navbar.classList.remove("sticky");
+    stick.classList.remove("sticky");
   }
 }
