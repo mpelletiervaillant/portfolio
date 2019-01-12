89691133a005c948c0975a0868c5b9fd0d0ad4e2
Melany Pelletier <melany.pelletier@gmail.com>
2019-01-12 09:36:03 -0500

    Add animation and new content

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the navbar
-var stick = document.getElementById("stick");
-
-// Get the offset position of the navbar
-var sticky = stick.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    stick.classList.add("sticky")
-  } else {
-    stick.classList.remove("sticky");
-  }
-}
+(function($) {
+  /**
+   * Copyright 2012, Digital Fusion
+   * Licensed under the MIT license.
+   * http://teamdf.com/jquery-plugins/license/
+   *
+   * @author Sam Sehnert
+   * @desc A small plugin that checks whether elements are within
+   *     the user visible viewport of a web browser.
+   *     only accounts for vertical position, not horizontal.
+   */
+  $.fn.visible = function(partial) {
+      var $t            = $(this),
+          $w            = $(window),
+          viewTop       = $w.scrollTop(),
+          viewBottom    = viewTop + $w.height(),
+          _top          = $t.offset().top,
+          _bottom       = _top + $t.height(),
+          compareTop    = partial === true ? _bottom : _top,
+          compareBottom = partial === true ? _top : _bottom;
+    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+  };
+})(jQuery);
+$(window).scroll(function(event) {
+  $('.intro-text, .slide-element, .main-preview-img, .tt-final-preview-img').each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("slide-in");
+    } else {
+      el.removeClass("slide-in");
+    }
+  });
+});
